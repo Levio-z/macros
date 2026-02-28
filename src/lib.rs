8c924052f9fd0d03cb087613b5ce62ce63c836ac
Levio-Z <67247011+Levio-z@users.noreply.github.com>
2026-02-28 15:56:46 +0800

    refactor: extract process_enum_from to separate module

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,9 @@
-// for enum,we'd like to generate From impls for each variant
+mod enum_from;
+
 use proc_macro::TokenStream;
-use quote::quote;
 #[proc_macro_derive(EnumFrom)]
 pub fn derive_enum_from(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as syn::DeriveInput);
     println!("{:#?}", input);
-    // get the enum name
-    let enum_name = input.ident;
-    let generics = &input.generics;
-    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-    // get enum variants
-    let variants = match input.data {
-        syn::Data::Enum(data_enum) => data_enum.variants,
-        _ => panic!("EnumFrom derive only works on enums"),
-    };
-    let from_impls = variants.iter().filter_map(|variant| {
-        let variant_name = &variant.ident;
-        let fields = &variant.fields;
-        match fields {
-            syn::Fields::Unnamed(unnamed) => {
-                if let Some(field) = unnamed.unnamed.first() {
-                    let inner_type = &field.ty;
-                    Some(quote! {
-                        impl #impl_generics From<#inner_type> for #enum_name #ty_generics #where_clause {
-                            fn from(v: #inner_type) -> Self {
-                                Self::#variant_name(v)
-                            }
-                        }
-                    })
-                } else {
-                    None
-                }
-            }
-            syn::Fields::Unit => Some(quote! {}),
-            _ => None,
-        }
-    });
-    quote! { #(#from_impls)* }.into()
+    enum_from::process_enum_from(&input)
 }
